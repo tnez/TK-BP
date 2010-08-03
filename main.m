450d0d713f7a2aa96e54ee16170d1bea5326c6c3
Travis Nesland <tnesland@gmail.com>
2010-08-03 16:35:50 -0400

    Initial commit.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BP
+//
+//  Created by Travis Nesland on 8/3/10.
+//  Copyright University of Kentucky 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
